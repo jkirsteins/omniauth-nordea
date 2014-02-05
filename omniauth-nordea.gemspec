8f56a9005878e4ea2777f0e93ba7ba97f77d16c6
Kristaps Erglis <kristaps.erglis@gmail.com>
2014-02-05 14:16:16 +0200

    i18n, strategy and spec fixes

--- omniauth-nordea.gemspec
+++ omniauth-nordea.gemspec
@@ -4,23 +4,27 @@ $LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
 require 'omniauth/nordea/version'
 
 Gem::Specification.new do |gem|
-  gem.name          = "omniauth-nordea"
+  gem.name          = 'omniauth-nordea'
   gem.version       = Omniauth::Nordea::VERSION
-  gem.authors       = ["Jānis Kiršteins"]
-  gem.email         = ["janis@montadigital.com"]
+  gem.authors       = ['Jānis Kiršteins', 'Kristaps Ērglis']
+  gem.email         = ['janis@montadigital.com', 'kristaps.erglis@gmail.com' ]
   gem.description   = %q{OmniAuth strategy for Nordea bank}
   gem.summary       = %q{OmniAuth strategy for Nordea bank}
-  gem.homepage      = ""
-  gem.license       = "MIT"
+  gem.homepage      = ''
+  gem.license       = 'MIT'
 
   gem.files         = `git ls-files`.split($/)
   gem.executables   = gem.files.grep(%r{^bin/}).map{ |f| File.basename(f) }
   gem.test_files    = gem.files.grep(%r{^(test|spec|features)/})
-  gem.require_paths = ["lib"]
+  gem.require_paths = ['lib']
 
   gem.add_runtime_dependency 'omniauth', '~> 1.0'
+  gem.add_runtime_dependency 'i18n'
+
   gem.add_development_dependency 'rack-test'
   gem.add_development_dependency 'rspec', '~> 2.7'
-  gem.add_development_dependency "bundler", "~> 1.3"
-  gem.add_development_dependency "rake"
+  gem.add_development_dependency 'bundler', '~> 1.3'
+  gem.add_development_dependency 'rake'
+  gem.add_development_dependency 'pry'
+
 end
--- spec/omniauth/strategies/nordea_spec.rb
+++ spec/omniauth/strategies/nordea_spec.rb
@@ -12,9 +12,8 @@ describe OmniAuth::Strategies::Nordea do
   end.to_app }
 
   context "request phase" do
-    before(:each) do
-      get "/auth/nordea"
-    end
+
+    before(:each) { get "/auth/nordea" }
 
     it "displays a single form" do
       expect(last_response.status).to eq(200)
@@ -25,7 +24,21 @@ describe OmniAuth::Strategies::Nordea do
       expect(last_response.body).to be_include("</form><script type=\"text/javascript\">document.forms[0].submit();</script>")
     end
 
-    EXPECTED_VALUES = {}
+    EXPECTED_VALUES = {
+      "A01Y_ACTION_ID" => "701",
+      "A01Y_VERS" => "0002",
+      "A01Y_RCVID" => RCVID,
+      "A01Y_LANGCODE" => "LV",
+      "A01Y_STAMP" => "yyyymmddhhmmssxxxxxx",
+      "A01Y_IDTYPE" => "02",
+      "A01Y_RETLINK" => "http://example.org/auth/nordea/callback?omniauth_status=success",
+      "A01Y_CANLINK" => "http://example.org/auth/nordea/callback?omniauth_status=cancelled",
+      "A01Y_REJLINK" => "http://example.org/auth/nordea/callback?omniauth_status=rejected",
+      "A01Y_KEYVERS" => "0001",
+      "A01Y_ALG" => "02",
+      "A01Y_MAC" => "c2e09d42e0eaf565ba1b14074f3bdae341b35bce"
+    }
+
     EXPECTED_VALUES.each_pair do |k,v|
       it "has hidden input field #{k} => #{v}" do
         expect(last_response.body.scan(
@@ -36,6 +49,32 @@ describe OmniAuth::Strategies::Nordea do
   end
 
   context "callback phase" do
+    let(:auth_hash){ last_request.env['omniauth.auth'] }
+
+    context "with valid response" do
+      before do
+        post :'/auth/nordea/callback',
+          "B02K_VERS" => "0002",
+          "B02K_TIMESTMP" => "2002014020513320773",
+          "B02K_IDNBR" => "f26402f2250340dba8b24c8498fd8c58",
+          "B02K_STAMP" => "yyyymmddhhmmssxxxxxx",
+          "B02K_CUSTNAME" => "Example User",
+          "B02K_KEYVERS" => "0001",
+          "B02K_ALG" => "02",
+          "B02K_CUSTID" => "123456-12345",
+          "B02K_CUSTTYPE" => "01",
+          "B02K_MAC" => "852E3207E143677B6E622DDF1D27B13979DB8C67"
+      end
+
+      it "sets the correct uid value in the auth hash" do
+        expect(auth_hash.uid).to eq("123456-12345")
+      end
+
+      it "sets the correct info.full_name value in the auth hash" do
+        expect(auth_hash.info.full_name).to eq("Example User")
+      end
+    end
+
   end
 
 end
